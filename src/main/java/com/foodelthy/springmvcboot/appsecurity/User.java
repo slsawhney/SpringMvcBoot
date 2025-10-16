e4f033f4408f2d4225dcbe1d2190ed0e0eec1811
Shyamlal Sawhney <slsawhney@yahoo.com>
2025-10-16 19:56:35 +0200

    SpringBoot Security

---
+++ src/main/java/com/foodelthy/springmvcboot/appsecurity/User.java
@@ -0,0 +1,41 @@
+package com.foodelthy.springmvcboot.appsecurity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    private String password;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
